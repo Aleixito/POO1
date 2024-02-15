60603ade4538fce6454f46e31d39dd386bf9ebe9
Aleixito <aleixtomu@gmail.com>
2024-02-15 18:42:51 +0100

    exercise2

--- ejercicio2.php
+++ ejercicio2.php
@@ -3,7 +3,32 @@
 <title>POO</title>
 </head>
 <body>
-<?php 
+<?php
+
+        abstract class Shape {
+            protected $width;
+            protected $height;
+
+        public function __construct($width, $height) {
+            $this->width = $width;
+            $this->height = $height;
+        }
+
+        abstract public function area();
+        }
+
+        class Rectangle extends Shape {
+            
+        public function area() {
+            return $this->width * $this->height;
+        }
+        }
+
+        class Triangle extends Shape {
+        public function area() {
+            return 0.5 * $this->width * $this->height;
+        }
+        }
 
 ?>
 </body>
